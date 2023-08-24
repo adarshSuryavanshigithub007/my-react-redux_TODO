e9e0c603ab614abdd0373228a14e4ae8ee2bebb2
Your Name <you@example.com>
2023-08-25 00:37:01 +0530

    added redux path

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import AddTodo from './component/addTodo';
 
 function App() {
   return (
     <div className="App">
-      <h1>hello</h1>
+      <AddTodo/>
     </div>
   );
 }
