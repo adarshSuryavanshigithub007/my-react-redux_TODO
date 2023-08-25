f6a39b9d5d9b50f9d3cdfc36dcccdf2acfd874e9
Your Name <you@example.com>
2023-08-25 12:41:47 +0530

    removed all code

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
+
 import './App.css';
-import AddTodo from './component/addTodo';
+
 
 function App() {
   return (
     <div className="App">
-      <AddTodo/>
+
     </div>
   );
 }
